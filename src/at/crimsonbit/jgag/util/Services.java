2e9d047fae88f8049a3040dcd259fffddf951b8e
NeonArtworks <flori.wagner@hotmail.com>
2017-06-06 15:31:27 +0200

    JGag Core

--- src/at/crimsonbit/jgag/util/Services.java
+++ src/at/crimsonbit/jgag/util/Services.java
@@ -7,18 +7,15 @@ package at.crimsonbit.jgag.util;
  *
  */
 public enum Services {
-	API("http://api.9gag.com"),
-	COMMENT_CDN("http://comment-cdn.9gag.com"),
-	NOTIFY("http://notify.9gag.com"),
-	AD("http://ad.9gag.com"),
-	ADMIN("http://admin.9gag.com");
-	
+	API("http://api.9gag.com"), COMMENT_CDN("http://comment-cdn.9gag.com"), NOTIFY("http://notify.9gag.com"), AD(
+			"http://ad.9gag.com"), ADMIN("http://admin.9gag.com");
+
 	private String url;
-	
+
 	private Services(String url) {
 		this.url = url;
 	}
-	
+
 	public String getURL() {
 		return url;
 	}
