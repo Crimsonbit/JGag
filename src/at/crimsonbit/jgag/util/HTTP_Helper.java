be59f7d3a1c06ca88662fb0e875d68c786f48dbf
Alexander Daum <daumalex10200@gmail.com>
2017-06-06 15:43:01 +0200

    Created HTTP_Helper

---
+++ src/at/crimsonbit/jgag/util/HTTP_Helper.java
@@ -0,0 +1,21 @@
+package at.crimsonbit.jgag.util;
+
+import java.util.Map;
+
+public class HTTP_Helper {
+	public String get(String url) {
+		return null;
+	}
+	
+	public String get(String url, Map<String, String> args) {
+		return null;
+	}
+	
+	public String post(String url) {
+		return null;
+	}
+	
+	public String post(String url, Map<String, String> args) {
+		return null;
+	}
+}
