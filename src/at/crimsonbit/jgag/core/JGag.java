eb4c2b847ac4b9fc1f3b4ae5979922f95d16b38d
NeonArtworks <flori.wagner@hotmail.com>
2017-06-06 16:09:52 +0200

    Core Changes

--- src/at/crimsonbit/jgag/core/JGag.java
+++ src/at/crimsonbit/jgag/core/JGag.java
@@ -10,31 +10,18 @@ public class JGag {
 	private String client_name;
 	private String client_secret;
 	private MessageDigest md;
+
 	public JGag() {
 	}
 
-	public void login(String client_name, String client_secret) throws NoSuchAlgorithmException, UnsupportedEncodingException {
+	public void login(String client_name, String client_secret)
+			throws NoSuchAlgorithmException, UnsupportedEncodingException {
 		this.client_name = client_name;
-		
-		//create instance of messagedigest
+
+		// create instance of messagedigest
 		md = MessageDigest.getInstance("MD5");
-		
 		byte[] secret_in = client_secret.getBytes("UTF-8");
-		byte[] secret_out = md.digest(secret_in);
-		
+		byte[] secret_out = md.digest(secret_in); // MD5
 		this.client_secret = new String(secret_out, StandardCharsets.UTF_8);
-		System.out.println(this.client_secret);
 	}
-
-	public static void main(String[] args) {
-		JGag g = new JGag();
-
-		try {
-			g.login("NeonArtworks", "awd");
-		} catch (NoSuchAlgorithmException | UnsupportedEncodingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-
 }
